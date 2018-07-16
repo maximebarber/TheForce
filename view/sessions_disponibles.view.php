f5cdaad2b75389f5f97f7e167fed69ff055f7ff0
Maxime <maximebarber@gmail.com>
2018-07-16 14:42:29 +0200

    Responsive bootstrap cards

--- view/sessions_disponibles.view.php
+++ view/sessions_disponibles.view.php
@@ -1,6 +1,6 @@
-<div id="cards">
-
-    <?php
+<?php
+    
+    echo '<div class="row">';
     
     //TANT QU'IL Y A DES SESSIONS ON LES AFFICHE SOUS FORME DE CARTES
     while ($session = $sessions->fetch()) {
@@ -16,19 +16,23 @@
         
         $nb_places_restantes = ($session['nb_places_theoriques']) - ($countStagiaire['countStagiaire']);
         
-        echo '<div class="card" style="width: 18rem;" id="card">
-  <img class="card-img-top" src="' . $session["img_session"] . '" alt="study-img">
-  <div class="card-body">
-    <h5 class="card-title">' . $session["intitule_session"] . ' <span class="badge badge-pill badge-primary"> ' . $nb_places_reservees . '/' . $session['nb_places_theoriques'] .'</span></h5>  
-         
-    <p class="card-text">Du <strong>' . $session["date_debut"] . '</strong> au <strong>' . $session['date_fin'] . '</strong></p>
+        echo '<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+                <div class="card" id="card">
+                    <img class="card-img-top" src="' . $session["img_session"] . '" alt="study-img">
+                    <div class="card-body">
+                      <h5 class="card-title">' . $session["intitule_session"] . ' <span class="badge badge-pill badge-primary"> ' . $nb_places_reservees . '/' . $session['nb_places_theoriques'] .'</span></h5>  
 
-    <p class="card-text">Nombre de places restantes : <span class="badge badge-pill badge-primary">' . $nb_places_restantes . '</span>  </p>
-    <a href=index.php?action=programme_session&amp;idSession=' . $session["id_session"] . ' class="btn btn-primary">Plus d\'infos</a>
-  </div>
-</div>';
+                      <p class="card-text">Du <strong>' . $session["date_debut"] . '</strong> au <strong>' . $session['date_fin'] . '</strong></p>
+
+                      <p class="card-text">Places restantes : <span class="badge badge-pill badge-primary">' . $nb_places_restantes . '</span>  </p>
+                      <a href=index.php?action=programme_session&amp;idSession=' . $session["id_session"] . ' class="btn btn-primary">Plus d\'infos</a>
+                    </div>
+                </div>
+              </div>';
+
     }
-
+    
+            
     echo '</div>';
 
 //PERMET DE STOPPER CONNECTION A LA BDD
