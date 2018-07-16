a02c43f97b7e6c102f7ec20f8ed49a52375f00d0
Unknown <joseph.paredes@gmail.com>
2018-07-16 16:29:01 +0200

    favicon et head
    le favicon et le titre du site internet sont ajoutées.

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+      <head>
+              <title> TheForce </title>
+            <link rel="icon" type="image/png" href="favicon.png" />
+      </head>
+</html>
+
+
 <link rel="stylesheet" href="CSS/style.css" />
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
